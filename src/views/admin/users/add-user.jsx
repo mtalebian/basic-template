2215e8a9d158a050896c9ca69130ba3579e1f9fb
aramrahmani <ar.rahmani@ikco.ir>
2021-10-17 14:21:16 +0330

    initial change

--- src/views/admin/users/add-user.jsx
+++ src/views/admin/users/add-user.jsx
@@ -7,155 +7,167 @@ import { notify } from "../../../components/basic/notify";
 import { Form, Formik } from "formik";
 import { BasicInput } from "../../../components/basic-form/basic-input";
 import { userManagmentApi } from "../../../api/user-managment-api";
-import { TabContainer, TabContent, TabPane,Tab,Nav,Col,Row,NavItem} from "react-bootstrap";
+import { Tab } from "react-bootstrap";
 
 export const AddUser = ({ onGoBack, ...props }) => {
-    const { t } = useTranslation();
-    const [busy, setBusy] = useState(false);
-    const formRef = useRef();
-    const [windowsAuth, setAuthType] = useState("true");
-    const [selectedTab, setTab] = useState('first');
-    
+  const { t } = useTranslation();
+  const [busy, setBusy] = useState(false);
+  const formRef = useRef();
+  const [windowsAuth, setAuthType] = useState("true");
 
-    const onSaveClick = (e) => {
-        if (!formRef.current) return false;
-        if (!formRef.current.isValid) return false;
-        var values = formRef.current.values;
-        setBusy(true);
-        userManagmentApi
-            .insertUser(values)
-            .then((x) => {
-                setBusy(false);
-                notify.info(t("changes-are-saved"));
-                onGoBack();
-            })
-            .catch((ex) => {
-                setBusy(false);
-                notify.error(ex);
-            });
+  const onSaveClick = (e) => {
+    if (!formRef.current) return false;
+    if (!formRef.current.isValid) return false;
+    var values = formRef.current.values;
+    setBusy(true);
+    userManagmentApi
+      .insertUser(values)
+      .then((x) => {
+        setBusy(false);
+        notify.info(t("changes-are-saved"));
+        onGoBack();
+      })
+      .catch((ex) => {
+        setBusy(false);
+        notify.error(ex);
+      });
+  };
+  const GeneralUserInfo = () => {
+    const inputStyle = {
+      maxWidth: 250,
     };
-   const UserInfo=()=>{
-        return(
-            <div className="row">
-            <div className="col-md-6">
-            <BasicInput name="firstName" label={t("first-name")} labelSize="4" autoComplete="off" autoFocus />
-            <BasicInput
-                name="lastName"
-                label={t("last-name")}
-                labelSize="4"
-                autoComplete="off"
-                // style={{ maxWidth: 150 }}
-            />
-            <BasicInput name="userName" label={t("user-name")} labelSize="4" autoComplete="off" />
-            <BasicInput name="nationalCode" label={t("national-code")} labelSize="4" autoComplete="off" />
-            <BasicInput name="email" label={t("email")} labelSize="4" autoComplete="off" />
-            <BasicInput name="phoneNumber" label={t("phone-number")} labelSize="4" autoComplete="off" />
-            <div className="row mb-2">
-                <label class="form-label text-start text-md-end col-form-label col-12 col-md-4">{t("auth-type")}</label>
-                <div className="col-md-8 m-s-n2">
-                    <bd.Toggle
-                        color="primary"
-                        dense
-                        radio
-                        size="sm"
-                        name="windowsAuthenticate"
-                        value="true"
-                        label={t("windows")}
-                        labelClassName="m-e-2"
-                        model={windowsAuth}
-                        setModel={setAuthType}
-                    />
-                    <bd.Toggle
-                        color="primary"
-                        dense
-                        checked
-                        radio
-                        size="sm"
-                        name="windowsAuthenticate"
-                        value="false"
-                        label={t("form")}
-                        model={windowsAuth}
-                        setModel={setAuthType}
-                    />
-                </div>
-            </div>
-            {windowsAuth}
-            </div>
-            <div className="col-md-6">
-            {windowsAuth=="false" &&(
-            <>
-             <BasicInput name="password" type="password" label={t("password")} labelSize="4" autoComplete="off" />
-             <BasicInput name="repeatePassword" type="password" label={t("repeate-password")} labelSize="4" autoComplete="off" />
-            </>
-           )}
-            </div>
-        </div>
-        )};
-        
     return (
-        <div>
-            <div className="border-bottom">
-                <bd.Toolbar className="container">
-                    <bd.Button variant="icon" onClick={onGoBack} size="md" edge="start" className="m-e-2">
-                        <icons.ArrowBackIos className="rtl-rotate-180" />
-                    </bd.Button>
-                    <h5>{t("new-user")}</h5>
-                    <div className="flex-grow-1" />
-
-                    <bd.Button color="primary" disabled={busy} onClick={() => formRef.current.submitForm()}>
-                        {busy && <div className="m-e-2 spinner-border spinner-border-sm"></div>}
-                        <span>{t("save-changes")}</span>
-                    </bd.Button>
-                </bd.Toolbar>
-            </div>
-
-            <div className="container pt-3">
-                <Formik
-                    initialValues={{
-                        firstName: "",
-                        lastName: "",
-                        userName: "",
-                        nationalCode: "",
-                        email:"",
-                        password: "",
-                        repeatePassword:"",
-                        phoneNumber:"",
-                        windowsAuthenticate: true,
-                    }}
-                    validationSchema={yup.object({
-                        firstName: yup.string().required("required"),
-                        lastName: yup.string().required("required"),
-                        userName: yup.string().required("required"),
-                        nationalCode:yup.string().required("required"),
-                    })}
-                    onSubmit={onSaveClick}
-                    innerRef={formRef}
-                >
-                    <Form>
-                        <div className="row">
-                            <div className="col-md-12">
-                             <Tab.Container defaultActiveKey="first">
-                            <bd.AppBar color="default">
-                                    <bd.TabStrip shade="primary" indicatorColor="primary" >
-                                        <bd.TabStripItem eventKey="first" >{t("user-info-tab")}</bd.TabStripItem>
-                                        <bd.TabStripItem eventKey="second">{t("user-role-tab")}</bd.TabStripItem>
-                                        <bd.TabStripItem eventKey="third">{t("password-manage-tab")}</bd.TabStripItem>
-                                    </bd.TabStrip>
-                            </bd.AppBar>
-                             <Tab.Content className="mt-4">
-                              <Tab.Pane eventKey="first">
-                                  <UserInfo/>
-                              </Tab.Pane>
-                              <Tab.Pane eventKey="second">
-                                      12121
-                               </Tab.Pane>
-                                </Tab.Content>
-                            </Tab.Container>
-                            </div>
-                        </div>
-                    </Form>
-                </Formik>
-            </div>
+      <div className="row">
+        <div className="col-md-6">
+          <BasicInput name="firstName" label={t("first-name")} labelSize="2" autoComplete="off" autoFocus style={inputStyle} />
+          <BasicInput name="lastName" label={t("last-name")} labelSize="2" autoComplete="off" style={inputStyle} />
+          <BasicInput name="userName" label={t("user-name")} labelSize="2" autoComplete="off" style={inputStyle} />
+          <BasicInput name="nationalCode" label={t("national-code")} labelSize="2" autoComplete="off" style={inputStyle} />
+          <BasicInput name="email" label={t("email")} labelSize="2" autoComplete="off" style={inputStyle} />
+          <BasicInput name="phoneNumber" label={t("phone-number")} labelSize="2" autoComplete="off" style={inputStyle} />
         </div>
+      </div>
     );
+  };
+
+  const AuthenticationType = () => {
+    const inputStyle = {
+      maxWidth: 250,
+    };
+    return (
+      <div className="row mb-2">
+        <div className="col-md-6">
+          <label class="form-label text-start text-md-end col-form-label col-12 col-md-2">{t("auth-type")}</label>
+          <bd.Toggle
+            color="primary"
+            dense
+            radio
+            size="sm"
+            name="windowsAuthenticate"
+            value="true"
+            label={t("windows")}
+            labelClassName="m-e-2"
+            model={windowsAuth}
+            setModel={setAuthType}
+          />
+          <bd.Toggle
+            color="primary"
+            dense
+            checked
+            radio
+            size="sm"
+            name="windowsAuthenticate"
+            value="false"
+            label={t("form")}
+            model={windowsAuth}
+            setModel={setAuthType}
+          />
+          {windowsAuth == "false" && (
+            <div className="mt-4">
+              <BasicInput name="password" type="password" label={t("password")} labelSize="3" autoComplete="off" style={inputStyle} />
+              <BasicInput name="repeatePassword" type="password" label={t("repeate-password")} labelSize="3" autoComplete="off" style={inputStyle} />
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="border-bottom">
+        <bd.Toolbar className="container">
+          <bd.Button variant="icon" onClick={onGoBack} size="md" edge="start" className="m-e-2">
+            <icons.ArrowBackIos className="rtl-rotate-180" />
+          </bd.Button>
+          <h5>{t("new-user")}</h5>
+          <div className="flex-grow-1" />
+
+          <bd.Button color="primary" disabled={busy} onClick={() => formRef.current.submitForm()}>
+            {busy && <div className="m-e-2 spinner-border spinner-border-sm"></div>}
+            <span>{t("save-changes")}</span>
+          </bd.Button>
+          
+        </bd.Toolbar>
+      </div>
+
+      <div className="container">
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            userName: "",
+            nationalCode: "",
+            email: "",
+            password: "",
+            repeatePassword: "",
+            phoneNumber: "",
+            windowsAuthenticate: "true",
+          }}
+          validationSchema={yup.object({
+            firstName: yup.string().required("required"),
+            lastName: yup.string().required("required"),
+            userName: yup.string().required("required"),
+            nationalCode: yup.string().required("required"),
+            email: yup.string().email("email not valid"),
+            password: yup.string().when("windowsAuthenticate", {
+              is: (value) => value == "false",
+              then: yup.string().required("required"),
+            }),
+            repeatePassword: yup.string().when("password", {
+              is: (value) => value && value.length > 0,
+              then: yup.string().required("required"),
+            }),
+            phoneNumber: yup.string().matches(/^[0-9]{11}$/, "Must be exactly 11 digits"),
+          })}
+          onSubmit={onSaveClick}
+          innerRef={formRef}
+        >
+          <Form>
+            <div className="row">
+              <div className="col-md-12">
+                <Tab.Container defaultActiveKey="first">
+                  <bd.AppBar color="default" shadow="0" color="inherit">
+                    <bd.TabStrip shade="primary" indicatorColor="primary">
+                      <bd.TabStripItem eventKey="first">{t("general-info-tab")}</bd.TabStripItem>
+                      <bd.TabStripItem eventKey="second">{t("authentication-type-tab")}</bd.TabStripItem>
+                      <bd.TabStripItem eventKey="third">{t("role-manage-tab")}</bd.TabStripItem>
+                    </bd.TabStrip>
+                  </bd.AppBar>
+                  <Tab.Content className="mt-4">
+                    <Tab.Pane eventKey="first">
+                      <GeneralUserInfo />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <AuthenticationType />
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
 };
--- src/views/admin/users/users-app.jsx
+++ src/views/admin/users/users-app.jsx
@@ -10,105 +10,104 @@ import { userManagmentApi } from "../../../api/user-managment-api";
 import { notify } from "../../../components/basic/notify";
 
 export const UsersApp = ({ ...props }) => {
-    const { t } = useTranslation();
-    const [insertMode, setInsertMode] = useState(false);
-    const account = useAccount();
-    const [users, setUsers] = useState(null);
-    const tableRef = React.useRef();
-    const columns = React.useMemo(
-        () => [
-            { Header: t("id"), accessor: "id" },
-            { Header: t("user-name"), accessor: "userName" },
-            { Header: t("first-name"), accessor: "firstName" },
-            { Header: t("last-name"), accessor: "lastName" },
-            { Header: t("windows"), accessor: "windowsAuthenticate" },
-            { Header: t("national-code"), accessor: "nationalCode" },
-        ],
-        []
-    );
+  const { t } = useTranslation();
+  const [insertMode, setInsertMode] = useState(false);
+  const account = useAccount();
+  const [users, setUsers] = useState(null);
+  const tableRef = React.useRef();
+  const columns = React.useMemo(
+    () => [
+      { Header: t("id"), accessor: "id" },
+      { Header: t("user-name"), accessor: "userName" },
+      { Header: t("first-name"), accessor: "firstName" },
+      { Header: t("last-name"), accessor: "lastName" },
+      { Header: t("windows"), accessor: "windowsAuthenticate" },
+      { Header: t("national-code"), accessor: "nationalCode" },
+    ],
+    []
+  );
 
-    const addUserOnClick = () => {
-        setInsertMode(true);
-    };
+  const addUserOnClick = () => {
+    setInsertMode(true);
+  };
 
-    useEffect(() => {
-        if (!users && account.isConnected()) {
-        userManagmentApi
-            .getUsers()
-            .then((x) => {       
-                setUsers(x);
-            })
-            .catch((ex) => {
-                notify.error(ex);
-            });
-        }
-    });
+  useEffect(() => {
+    if (!users && account.isConnected()) {
+      userManagmentApi
+        .getUsers()
+        .then((x) => {
+          setUsers(x);
+        })
+        .catch((ex) => {
+          notify.error(ex);
+        });
+    }
+  });
 
-    return (
+  return (
+    <>
+      {!insertMode && !users && <div className="middle d-flex h-100">L O D I N G . . .</div>}
+      {!insertMode && users && (
         <>
-            {!insertMode && !users && <div className="middle d-flex h-100">L O D I N G . . .</div>}
-            {!insertMode && users && (
-                <>
-                    <div className="border-bottom">
-                        <bd.Toolbar className="container">
-                            <bd.Button variant="text" onClick={addUserOnClick}>
-                                <icons.PersonAddAlt className="size-lg" />
-                            </bd.Button>
+          <div className="border-bottom">
+            <bd.Toolbar className="container">
+              <bd.Button variant="text" onClick={addUserOnClick}>
+                <icons.PersonAddAlt className="size-lg" />
+              </bd.Button>
 
-                            <bd.Button variant="text">
+              {/* <bd.Button variant="text">
                                 <icons.Person className="size-lg" />
                                 {t("View user")}
-                            </bd.Button>
-                        </bd.Toolbar>
-                    </div>
-                    <div className="container">
-                        
-                        <TableTitlebar title="Users" />
-                        <Table
-                            //className="w-100"
-                            columns={columns}
-                            //defaultColumn={defaultColumn}
-                            data={users}
-                            //updateData={updateMyData}
-                            //skipReset={skipResetRef.current}
-                            //enablePaging={enablePaging}
-                            //enableGrouping={enableGrouping}
-                            //enableSorting={enableSorting}
-                            //showTableInfo={showTableInfo}
-                            //showSummary={showSummary}
-                            //showColumnFilter={showColumnFilter}
-                            //hideColumns={}
-                            //showFooter={showFooter}
-                            //showPageSize={true}
-                            //border=""
-                            editable={false}
-                            //clickAction="toggle"
-                            //hideCheckbox={hideCheckbox}
-                            //selectionMode="single"
-                            //messages={messages}
-                            tableRef={tableRef}
-                            //tableClassName="w-100"
-                            //
-                            title="Columns"
-                            expandableTitlebar={true}
-                            showRowsCount={true}
-                            titlebarSize="md"
-                            titlebarColor="secondary"
-                            //
-                            defaultPageSize={5}
-                            onStateChanged={(state) => {
-                                console.log(state);
-                            }}
-                        />
-                    </div>
-                </>
-            )}
-
-            {insertMode && <AddUser onGoBack={() => setInsertMode(false)} />}
+                            </bd.Button> */}
+            </bd.Toolbar>
+          </div>
+          <div className="container">
+            <TableTitlebar title="Users" />
+            <Table
+              //className="w-100"
+              columns={columns}
+              //defaultColumn={defaultColumn}
+              data={users}
+              //updateData={updateMyData}
+              //skipReset={skipResetRef.current}
+              //enablePaging={enablePaging}
+              //enableGrouping={enableGrouping}
+              //enableSorting={enableSorting}
+              //showTableInfo={showTableInfo}
+              //showSummary={showSummary}
+              //showColumnFilter={showColumnFilter}
+              //hideColumns={}
+              //showFooter={showFooter}
+              //showPageSize={true}
+              //border=""
+              editable={false}
+              //clickAction="toggle"
+              //hideCheckbox={hideCheckbox}
+              //selectionMode="single"
+              //messages={messages}
+              tableRef={tableRef}
+              //tableClassName="w-100"
+              //
+              title="Columns"
+              expandableTitlebar={true}
+              showRowsCount={true}
+              titlebarSize="md"
+              titlebarColor="secondary"
+              //
+              defaultPageSize={5}
+              onStateChanged={(state) => {
+                console.log(state);
+              }}
+            />
+          </div>
         </>
-    );
+      )}
+
+      {insertMode && <AddUser onGoBack={() => setInsertMode(false)} />}
+    </>
+  );
 };
 
 UsersApp.Appbar = {
-    title: "users",
+  title: "users",
 };
