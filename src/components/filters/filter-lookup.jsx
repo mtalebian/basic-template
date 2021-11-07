222961481ec65ff13aac42b7f66ebe1716fd0458
Mahdi Talebian <talebian73@gmail.com>
2021-11-08 01:01:59 +0330

    filter

--- src/components/filters/filter-lookup.jsx
+++ src/components/filters/filter-lookup.jsx
@@ -17,7 +17,7 @@ export const FilterLookup = ({ name, title, show, setShow, ...props }) => {
     const values = field.value ? field.value : [];
 
     return (
-        <Modal show={show} onHide={hide} size="lg" fullscreen="lg-down" dialogClassName="my-0 py-lg-4 h-100" centered>
+        <Modal show={show} onHide={hide} size="xl" dialogClassName="my-0 py-lg-4 h-100" centered>
             <bd.Panel title={title} style={{ height: 40 }}></bd.Panel>
             <bd.TabStrip className="border-bottom p-s-2" size="sm" style={{ height: 44 }}>
                 <bd.TabStripItem>
@@ -28,27 +28,60 @@ export const FilterLookup = ({ name, title, show, setShow, ...props }) => {
                     <Text>define-conditions</Text>
                 </bd.TabStripItem>
             </bd.TabStrip>
-            <div className="bg-shade-3 flex-grow-1 flex-shrink-0 d-flex flex-column ">
-                <div className="bg-shade-3" style={{ minHeight }}>
-                    <h1>111</h1>
-                </div>
-                <div className="bg-shade-3 ">
-                    <div className="mx-4 mb-1 d-flex flex-wrap">
-                        <Text>selected items and conditions</Text>
+            <div className="bg-shade-3 flex-grow-1 flex-shrink-0 d-flex flex-column overflow-auto" style={{ maxHeight: minHeight + 50 }}>
+                <div className="bg-shade-3 p-3 overflow-hidden" style={{ minHeight }}>
+                    {values.map((x) => (
+                        <div className="container-flluid">
+                            <div className="row mb-3 gx-30">
+                                <div className="mb-2 col-12 col-sm-3">
+                                    <select className="form-select compact">
+                                        {["=", ">=", "<="].map((x) => (
+                                            <option value={x}>{x}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className="mb-2 col-12 col-sm-8">
+                                    <input className="form-control compact" value={x} />
+                                </div>
+                                <div className="mb-2 col-12 col-sm-1">
+                                    <bd.Button variant="text" color="primary" className="compact" size="sm">
+                                        <icons.Close />
+                                    </bd.Button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                    <div className="text-end mx-3">
+                        <bd.Button color="primary" className="compact" onClick={() => values.push()}>
+                            <Text>add</Text>
+                        </bd.Button>
                     </div>
-                    <div className="mx-4 mb-3 border px-2 py-1">{values}</div>
+                </div>
+                AAAAAAAAAAA
+                <div className="bg-shade-3 ">
+                    <div className="mx-4 mb-1">
+                        <Text>selected-items-and-conditions</Text>
+                    </div>
+                    <div className="mx-4 mb-3 border px-2 pt-1 bg-default rounded d-flex flex-wrap" style={{ height: "auto" }}>
+                        {values.map((x) => (
+                            <div className="border rounded px-2 m-e-2 mb-1 d-flex align-items-center cur-pointer hover-shade-5">
+                                {x}
+                                <icons.Close className="size-sm m-s-2" style={{ fontSize: 14 }} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
 
-            <Modal.Footer className="py-2 px-4">
-                <bd.Button color="secondary" onClick={hide} className="btn-compact">
+            <div className="py-2 px-4 border-top text-end">
+                <bd.Button color="secondary" onClick={hide} className="compact">
                     <Text>ok</Text>
                 </bd.Button>
 
-                <bd.Button variant="text" onClick={hide} className="btn-compact">
+                <bd.Button variant="text" onClick={hide} className="compact">
                     <Text>cancel</Text>
                 </bd.Button>
-            </Modal.Footer>
+            </div>
         </Modal>
     );
 };
